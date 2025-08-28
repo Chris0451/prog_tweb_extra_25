1b2097908cafd465d76b33b735c80c2dcd87462f
Chris0451 <dcchris56@gmail.com>
2025-08-28 23:02:24 +0200

    Update app.php

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://tweban.dii.univpm.it/~grp_52/laraProject/public'),
 
     /*
     |--------------------------------------------------------------------------
