edc92d40915935d26f3185f4b095a8cb70d721c4
Chris0451 <dcchris56@gmail.com>
2025-08-29 17:48:59 +0200

    Update productImg.blade.php

--- resources/views/layouts/users_layouts/admin/helpers/productImg.blade.php
+++ resources/views/layouts/users_layouts/admin/helpers/productImg.blade.php
@@ -6,5 +6,6 @@
 @endphp
 @if ($bool_img)
     <img src="{{ asset('images/placeholder.jpg') }}" {!! $attrs !!}>
+@else
+    <img src="{{ asset('storage/images/products/'.$imgFile) }}" {!! $attrs !!}>
 @endif
-<img src="{{ asset('storage/images/products/'.$imgFile) }}" {!! $attrs !!}>
