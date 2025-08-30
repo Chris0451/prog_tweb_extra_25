bf8e03a26cd10d173fc5e1565da71f4edffa2c5f
Chris0451 <dcchris56@gmail.com>
2025-08-30 18:08:22 +0200

    Add PDF link to hero section and upload document
    Uploaded 'documentazione_tweb_extra_25.pdf' to the public/pdf directory and added a link to this document in the hero section of the home layout. This provides users with direct access to the referenced PDF.

--- resources/views/layouts/home_layouts/hero.blade.php
+++ resources/views/layouts/home_layouts/hero.blade.php
@@ -1,4 +1,5 @@
 <div class="hero">
         <h1>Benvenuti nel portale ufficiale di assistenza</h1>
         <p>Consulta il catalogo, risolvi problemi tecnici o trova il centro più vicino.</p>
+        <p><a href="{{ asset('pdf/documentazione_tweb_extra_25.pdf') }}" target="_blank">LINK ALLA RELAZIONE</a></p>
 </div>
